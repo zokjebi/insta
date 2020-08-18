62ce92db42e1a824ec919d7391965fa8dadf3caf
zokjebi <myimgman@gmail.com>
2020-08-18 10:37:10 +0900

    2020-08-18-01 zokjebi     스프링부트 JPA인스타그램 7강 Follow 1강 fetch api를 이용한 팔로우 구현 (41:45)

--- insta/src/main/java/com/cos/insta/controller/FollowController.java
+++ insta/src/main/java/com/cos/insta/controller/FollowController.java
@@ -1,5 +1,8 @@
 package com.cos.insta.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,15 +10,34 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cos.insta.model.Follow;
+import com.cos.insta.model.User;
+import com.cos.insta.repository.FollowRepository;
+import com.cos.insta.repository.UserRepository;
 import com.cos.insta.service.MyUserDetail;
 
 @Controller
 public class FollowController {
 	
+	@Autowired
+	private UserRepository mUserRepository;
+	
+	@Autowired
+	private FollowRepository mFollowRepository;
+	
 	@PostMapping("/follow/{id}")
 	public @ResponseBody Follow follow(@AuthenticationPrincipal MyUserDetail userDetail, 
 									   @PathVariable int id) {
 		
+		User fromUser = userDetail.getUser();
+		Optional<User> oToUser = mUserRepository.findById(id);
+		User toUser = oToUser.get();
 		
+		Follow follow = new Follow();
+		follow.setFromUser(fromUser);
+		follow.setToUser(toUser);
+		
+		mFollowRepository.save(follow);
+		
+		return follow;
 	}
 }
