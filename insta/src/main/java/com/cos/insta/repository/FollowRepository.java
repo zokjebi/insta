7f82ce92507ce963ffa40b1ad337454ab836fb24
zokjebi <myimgman@gmail.com>
2020-08-18 11:58:29 +0900

    2020-08-18-03 zokjebi     스프링부트 JPA인스타그램 7강 Follow 1강 fetch api를 이용한 팔로우 구현 (1:13:10)

--- insta/src/main/java/com/cos/insta/repository/FollowRepository.java
+++ insta/src/main/java/com/cos/insta/repository/FollowRepository.java
@@ -3,6 +3,7 @@ package com.cos.insta.repository;
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.cos.insta.model.Follow;
 
@@ -11,4 +12,7 @@ public interface FollowRepository extends JpaRepository<Follow, Integer>{
 	// unfollow
 	@Transactional
 	int deleteByFromUserIdAndToUserId(int fromUserId, int toUserId);
+	
+	// 팔로우 유무
+	int countByFromUserIdAndToUserId(int fromUserId, int toUserId);
 }
